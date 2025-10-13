1f34f5b8a553f5810806be11cf266a654bb1dc97
MadoctheHadoc <madockarthaus@hotmail.com>
2025-10-13 23:51:38 +0200

    Added big batch sbatch

---
+++ credit_batch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=run-experiments
+#SBATCH --output=%x_%j.out
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=30:00
+#SBATCH --reservation=terv92681
+
+# Load CUDA module (adjust version to match your system)
+deactivate
+rm -rf venv
+
+module purge
+module load 2022
+module load Python/3.10.4-GCCcore-11.3.0
+
+python --version
+python -m venv venv
+
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
