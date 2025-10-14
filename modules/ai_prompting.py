2b06f12eeb30ee8b90cd8b5b6ccaba68beead58b
MadoctheHadoc <madockarthaus@hotmail.com>
2025-10-14 22:52:51 +0200

    Minor changes

--- modules/ai_prompting.py
+++ modules/ai_prompting.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from llamainteract.llamapythonapi import LlamaInterface
+import inspect
 
 def generate_ai_answers(df, model_name, question_column):
     if model_name == "llama":
@@ -15,10 +16,11 @@ def run_llama(df, question_column):
     """
 
     llama = LlamaInterface()
+    print('FLorian Output code:::')
+    print(print(llama._class.module_))
+    print(inspect.getsource(LlamaInterface))
     df[question_column + "_answer_ai"] = df[question_column].apply(
         lambda x: llama.qa(x)["choices"][0]["text"].strip()
     )
     
     return df
-
-
