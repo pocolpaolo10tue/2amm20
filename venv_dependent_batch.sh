5ccf82c773b86790b604a7a1eab4b0d9079f44c1
MadoctheHadoc <madockarthaus@hotmail.com>
2025-10-15 11:18:12 +0200

    Loading some modules

--- venv_dependent_batch.sh
+++ venv_dependent_batch.sh
@@ -4,9 +4,17 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
+#SBATCH --partition=gpu_a100
 #SBATCH --time=00:30:00
-#SBATCH --partition=gpu_mig
-#SBATCH --reservation=terv92681
+#SBATCH --exclusive
+
+module load foss/2023a
+module load Python/3.9.16-foss-2023a
+module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
+
+which nvidia-smi
+nvidia-smi
+
 
 source venv/bin/activate
 python main.py
