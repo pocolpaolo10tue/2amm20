ebda5ae85f79611bf2a74a82649fb29378d73442
MadoctheHadoc <madockarthaus@hotmail.com>
2025-10-15 10:28:48 +0200

    Updated batch script.

--- venv_dependent_batch.sh
+++ venv_dependent_batch.sh
@@ -4,9 +4,9 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
-#SBATCH --partition=gpu_mig
 #SBATCH --time=00:30:00
-#SBATCH --exclusive
+#SBATCH --partition=gpu_mig
+#SBATCH --reservation=terv92681
 
 source venv/bin/activate
 python main.py
