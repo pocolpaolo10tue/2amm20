211e92faccb020d06d7f28f1555100405d918ce4
MadoctheHadoc <madockarthaus@hotmail.com>
2025-10-15 19:35:35 +0200

    h100 batch update

--- h100_batch.sh
+++ h100_batch.sh
@@ -11,12 +11,22 @@ module load 2023
 module load foss/2023a
 module load CUDA/12.1.1
 
-which nvcc
-nvcc --version
+# Remove broken venv (optional)
+rm -rf venv
 
+# Recreate venv on this node
+python -m venv venv
 source venv/bin/activate
-export CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCMAKE_CUDA_ARCHITECTURES=80;90"
-pip install --upgrade llama-cpp-python
 
+# Install dependencies
+export CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCMAKE_CUDA_ARCHITECTURES=80;90"
+pip install --upgrade pip
+pip install llama-cpp-python
+pip install -r requirements.txt   # if you have other dependencies
+
+# Test
+python -c "import llama_cpp; print('llama_cpp loaded')"
+
+# Run main script
 python main.py
 deactivate
