82f0a5db69d7b0ba671c2b5a2b5e1d5b8b4237a8
MadoctheHadoc <madockarthaus@hotmail.com>
2025-10-19 20:42:00 +0200

    final_prompts!

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from modules.ai_detector import run_ai_detector
 import multiprocessing as mp
 
 DATASET_NAME = "stackexchange_QA.parquet"
-PROMPT_FILE = "overall_prompt.csv"
+PROMPT_FILE = "final_prompt.csv"
 
 AI_MODEL_NAME = "llama"
 AI_DETECTOR_NAME = "binoculars"
 
-NUMBER_OF_QUESTIONS = 20
+NUMBER_OF_QUESTIONS = 100
 MIN_LENGTH_ANSWER = 100
 MAX_LENGTH_QUESTION = 1000
 
@@ -22,7 +22,7 @@ def main():
     df = clean_dataset(df, NUMBER_OF_QUESTIONS, MIN_LENGTH_ANSWER, MAX_LENGTH_QUESTION)
 
     print("=== Generating AI answers ===")
-    df = generate_ai_answers(df, AI_MODEL_NAME, "question")
+    df = generate_ai_answers(df, AI_MODEL_NAME, "question", repeat_penalty=1.6)
     
     print("=== Running AI detector on human text ===")
     df = run_ai_detector(AI_DETECTOR_NAME, df, "answer")
@@ -34,7 +34,7 @@ def main():
     df = create_question_with_prompt(df, prompt)
     
     print("=== Generating AI answer for question with prompt ===")
-    df = generate_ai_answers(df, AI_MODEL_NAME, "question_with_prompt")
+    df = generate_ai_answers(df, AI_MODEL_NAME, "question_with_prompt", repeat_penalty=1.6)
     
     print("=== Running AI detector for answer to question with prompt ===")
     df = run_ai_detector(AI_DETECTOR_NAME, df, "question_with_prompt_answer_ai")
@@ -160,4 +160,4 @@ def main_params_test():
 
 
 if __name__ == "__main__":
-    main_params_test()
+    main()
